12b18d0f8ed5578ee831b4581ddd4f804a761b74
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-04-27 15:23:22 -0400

    working inquire

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,50 @@
-const mysql = require("mysql");
+//lode required packages
 
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// save process arguments in a variable
 var args = process.argv[2];
 
+
+// Create a "Prompt" with a series of questions.
+inquirer
+    .prompt([
+        // Here we create basic text prompt.
+        {
+            type: "input",
+            message: "What is the itemID of the product you would like to purchase?",
+            name: "custPurchaseItemID"
+        },
+        {
+            type: "number",
+            message: "How many would you like to purchase?",
+            name: "custPurchaseQty"
+        },
+        // Here we ask the user to confirm.
+        {
+            type: "confirm",
+            message: "Are you sure:",
+            name: "confirm",
+            default: true
+        }
+
+    ])
+    .then(function (inquirerResponse) {
+        // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+        if (inquirerResponse.confirm) {
+            console.log("\nYou have selected to purchase " + inquirerResponse.custPurchaseQty + " units of item #" + inquirerResponse.custPurchaseItemID + "!\n");
+            //console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+        }
+        else {
+            console.log("\nThat's okay, come again when you are more sure.\n");
+        }
+    });
+
+
+
+
+// establish the db connection (and call a function)
 const connection = mysql.createConnection({
     host: "localhost",
     port: 8889, //this port needs to match the port of the sql server (don't get this confuses d with express homework)
@@ -15,23 +58,39 @@ connection.connect(function (err) {
     console.log("Connected as ID " + connection.threadId);
 
 
-  
-showProd();
-    // productFinder(args);
-    connection.end();
+
+    // showProd();
+    // // productFinder(args);
+    // connection.end();
 });
 
-function showProd(){
+// display all products function
+
+function showProd() {
 
     console.log("Selecting all Products...\n");
-                connection.query("SELECT * FROM products", function (err, res) {
-                    if (err) throw err;
-                    // Log all results of the SELECT statement
-                    console.table(res);
-                    //connection.end();  use ctrl c if you forget to end the connection in the .js file
-                });
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        //connection.end();  use ctrl c if you forget to end the connection in the .js file
+    });
 
 };
+
+
+
+
+
+
+
+
+    showProd();
+    // productFinder(args);
+    connection.end();
+
+
+
 
 
 // function productFinder(args) {
