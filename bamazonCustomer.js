5fe5ddc5d5ee53a6c5c08e07bd06267d92e66407
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-04-29 14:08:44 -0400

    overall clean up and comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,12 @@
-//load required packages
+// WELCOME TO BAMAZON!!! THE BEST CLI SHOP EVER!!!
 
-const mysql = require("mysql");
-const inquirer = require("inquirer");
 
-// save process arguments in a variable
-var args = process.argv[2];
 
-// establish the db connection (and call a function)
+// first things first - require mysql and inquirer
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// next - create connection to db
 const connection = mysql.createConnection({
     host: "localhost",
     port: 8889, //this port needs to match the port of the sql server (don't get this confuses d with express homework)
@@ -16,152 +16,84 @@ const connection = mysql.createConnection({
 });
 
 
+// Lets create the shop!
+function bShop() {
 
-
-// Create a "Prompt" with a series of questions.
-inquirer
-    .prompt([
-        // Here we create basic text prompt.
-        {
-            type: "input",
-            message: "What is the itemID of the product you would like to purchase?",
-            name: "custPurchaseItemID"
-        },
-        {
-            type: "number",
-            message: "How many would you like to purchase?",
-            name: "custPurchaseQty"
-        },
-        // Here we ask the user to confirm.
-        {
-            type: "confirm",
-            message: "Are you sure:",
-            name: "confirm",
-            default: true
-        }
-
-    ])
-    .then(function (inquirerResponse) {
-        // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-
-
-
-        if (inquirerResponse.confirm) {
-
-            var custItem = inquirerResponse.custPurchaseItemID;
-            var custQty =  inquirerResponse.custPurchaseQty;
-
-//put entire DB Connection within Inquirer
-
-            console.log("\nYou have selected to purchase " + inquirerResponse.custPurchaseQty + " units of item #" + inquirerResponse.custPurchaseItemID + "!\n");
-        }
-        else {
-            console.log("\nThat's okay, come again when you are more sure.\n");
-        }
-    });
-
-
-
-
-
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected as ID " + connection.threadId);
-
-
-
-    // showProd();
-    // // productFinder(args);
-    // connection.end();
-});
-
-// display all products function
-
-function showProd() {
-
-    console.log("Selecting all Products...\n");
-    connection.query("SELECT * FROM products", function (err, res) {
+    // now let us display the items for sale and their details
+    connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
-        // Log all results of the SELECT statement
+
         console.table(res);
-        //connection.end();  use ctrl c if you forget to end the connection in the .js file
+        console.log(' ');
+
+        // lets welcome the customer and ask them what they would like to purchase today!
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "WELCOME TO BAMAZON!!!   What is the itemId of the product you would like to purchase today?",
+                validate: function (value) {
+                    if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {  //found the isNAN if statement usage online!
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "qty",
+                message: "How many units would you like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value)) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+
+            // Now, lets see if there is enough quantity, if so, subtract the number of items from stock and total the price
+        ]).then(function (ans) {
+            var cartItem = (ans.id) - 1;
+            var cartItemQty = parseInt(ans.qty);
+            var grandTotal = parseFloat(((res[cartItem].price) * cartItemQty).toFixed(2));
+
+            //check if quantity is sufficient
+            if (res[cartItem].stockQty >= cartItemQty) {
+                //after purchase, updates quantity in Products
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    { stockQty: (res[cartItem].stockQty - cartItemQty) },
+                    { itemId: ans.id }
+                ], function (err, result) {
+                    if (err) throw err;
+                    console.log("PURCHASE COMPLETE! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+                });
+
+            } else {
+                console.log("We are so sorry, there's not enough in stock!");
+            }
+
+            reprompt();
+        })
+    })
+}
+
+// asks if they would like to purchase another item.  If so, start the shop again, if not say thanks and good bye!
+function reprompt() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "reply",
+        message: "Would you like to purchase another item?"
+    }]).then(function (ans) {
+        if (ans.reply) {
+            bShop();
+        } else {
+            console.log("THANK YOU FOR SHOPPING ON BAMAZON!!!  Have a great day!");
+            connection.end();
+        }
+
     });
+}
 
-};
-
-
-
-
-
-
-
-
-showProd();
-// productFinder(args);
-connection.end();
-
-
-
-
-
-// function productFinder(args) {
-//     console.log(args);
-//     if (args === "allProducts") {
-
-//         function readAllProducts() {
-//             console.log("Selecting all Products...\n");
-//             connection.query("SELECT * FROM products", function (err, res) {
-//                 if (err) throw err;
-//                 // Log all results of the SELECT statement
-//                 console.table(res);
-//                 //connection.end();  use ctrl c if you forget to end the connection in the .js file
-//             });
-
-//         };
-
-//         readAllProducts();
-
-//     } else if (args === "artistAlbumSongs") {
-
-//         function readAlbumSongs() {
-//             console.log("Selecting artist's songs...\n");
-//             connection.query('SELECT top5000.artist, title, album, top5000.year FROM top5000 INNER JOIN topAlbums ON top5000.artist = topAlbums.artist WHERE top5000.artist = "Sting" AND topAlbums.year = top5000.year;', function (err, res) {
-//                 if (err) throw err;
-//                 // Log all results of the SELECT statement
-//                 console.table(res);
-//                 //connection.end();  use ctrl c if you forget to end the connection in the .js file
-//             });
-//         };
-//         readAlbumSongs();
-
-//     } else if (args === "multiSongs") {
-
-
-//         function read2xArtistSongs() {
-//             console.log("Selecting songs from artist that appear more than once...\n");
-//             connection.query('SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 3 ', function (err, res) {
-//                 if (err) throw err;
-//                 // Log all results of the SELECT statement
-//                 console.table(res);
-//                 //connection.end();  use ctrl c if you forget to end the connection in the .js file
-//             });
-//         };
-//         read2xArtistSongs();
-
-//     } else if (args === "rangeSongs") {
-
-//         function readRangsOfSongs() {
-//             console.log("Selecting a range of songs between 1999 and 2010...\n");
-//             connection.query('SELECT * FROM top5000 WHERE year > "1999" AND year < "2010" ORDER BY year', function (err, res) {
-//                 if (err) throw err;
-//                 // Log all results of the SELECT statement
-//                 console.table(res);
-//                 //connection.end();  use ctrl c if you forget to end the connection in the .js file
-//             });
-
-//         };
-//         readRangsOfSongs();
-//     };
-
-// };
+bShop();
