9b2767ab899296e8e85ccdf6839f72bce22c4f07
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-04-29 14:01:09 -0400

    manually merged code

--- bamCust3.js
+++ bamCust3.js
@@ -1,12 +1,8 @@
-//load required packages
 
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-
-// save process arguments in a variable
-var args = process.argv[2];
-
-// establish the db connection (and call a function)
+//require mysql and inquirer
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+//create connection to db
 const connection = mysql.createConnection({
     host: "localhost",
     port: 8889, //this port needs to match the port of the sql server (don't get this confuses d with express homework)
@@ -15,118 +11,85 @@ const connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected as ID " + connection.threadId);
+function bShop(){
+//prints the items for sale and their details
+connection.query('SELECT * FROM products', function(err, res){
+  if(err) throw err;
+
+  console.table(res);
 
 
 
-    // showProd();
-    // // productFinder(args);
-    // connection.end();
-});
-
-
-
-
-// Create a "Prompt" with a series of questions.
-inquirer
-    .prompt([
-        // Here we create basic text prompt.
-        {
-            type: "input",
-            message: "What is the itemID of the product you would like to purchase?",
-            name: "custPurchaseItemID"
-        },
-        {
-            type: "number",
-            message: "How many would you like to purchase?",
-            name: "custPurchaseQty"
-        },
-        // Here we ask the user to confirm.
-        {
-            type: "confirm",
-            message: "Are you sure:",
-            name: "confirm",
-            default: true
+  console.log(' ');
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "WELCOME TO BAMAZON!!!   What is the itemId of the product you would like to purchase today?",
+      validate: function(value){
+        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){  //found the isNAN if statement usage online!
+          return true;
+        } else{
+          return false;
         }
+      }
+    },
+    {
+      type: "input",
+      name: "qty",
+      message: "How many units would you like to purchase?",
+      validate: function(value){
+        if(isNaN(value)){
+          return false;
+        } else{
+          return true;
+        }
+      }
+    }
+    ]).then(function(ans){
+      var cartItem = (ans.id)-1;
+      var cartItemQty = parseInt(ans.qty);
+      var grandTotal = parseFloat(((res[cartItem].price)*cartItemQty).toFixed(2));
 
-    ])
-    .then(function (inquirerResponse) {
-        // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+      //check if quantity is sufficient
+      if(res[cartItem].stockQty >= cartItemQty){
+        //after purchase, updates quantity in Products
+        connection.query("UPDATE products SET ? WHERE ?", [
+        {stockQty: (res[cartItem].stockQty - cartItemQty)},
+        {itemId: ans.id}
+        ], function(err, result){
+            if(err) throw err;
+            console.log("PURCHASE COMPLETE! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+        });
 
-        if (inquirerResponse.confirm) {
+ 
 
+      } else{
+        console.log("We are so sorry, there's not enough in stock!");
+        
 
-            //Trying to resolve why RES is not defined.
+      }
 
-                    // function showProd() {
+      reprompt();
+    })
+})
+}
 
-                    //     console.log("Selecting all Products...\n");
-                    //     connection.query("SELECT * FROM products", function (err, res) {
-                    //         if (err) throw err;
-                    //         // Log all results of the SELECT statement
-                    //         console.table(res);
-                    //         //connection.end();  use ctrl c if you forget to end the connection in the .js file
-                    //     });
-                    
-                    // };
-                    
-                    // showProd()
-
-
-            var custItem = inquirerResponse.custPurchaseItemID;
-            var custQty = inquirerResponse.custPurchaseQty;
-
-            // get the information of the chosen item
-            var chosenItem;
-            for (var i = 0; i < res.length; i++) {
-                if (res[i].item_name === inquirerResponse.product) {
-                    chosenItem = res[i];
-                }
-            }
-
-            // determine if bid was high enough but now determin if we have enough quantity
-            if (chosenItem.stockQty < parseInt(answer.purchaseQty)) {
-                // bid/(quantity) was high enough, so update db, let the user know, and start over
-
-                var newStockQty = chosenItem.stockQty - answer.purchaseQty;
-                connection.query(
-                    "UPDATE products SET ? WHERE ?",
-                    [
-                        {
-                            stockQty: newStockQty
-                        },
-                        {
-                            id: chosenItem.id
-                        }
-                    ],
-                    function (error) {
-                        if (error) throw err;
-                        console.log("Item " + chosenItem.productName + " purchased successfully!");
-                        buyProducts();
-                    }
-                );
-            }
-            else {
-                // we didn't have enough in stock, so apologize and start over
-                console.log("Our stock is too low. Try again...");
-                buyProducts();
-
-
-                connection.end();
-
-            }
-
-        };
+// asks if they would like to purchase another item
+function reprompt(){
+  inquirer.prompt([{
+    type: "confirm",
+    name: "reply",
+    message: "Would you like to purchase another item?"
+  }]).then(function(ans){
+    if(ans.reply){
+      bShop();
+    } else{
+      console.log("THANK YOU FOR SHOPPING ON BAMAZON!!!");
+      connection.end();
     }
 
+  });
+}
 
-
-
-
-
-
-
-
-    )
+bShop();
